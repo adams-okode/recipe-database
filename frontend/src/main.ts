3088c8c232e3430f6a80d030fd85999f715b9422
Adams Okode <adamsokode@gmail.com>
2024-08-06 22:58:31 +0300

    Set up fron end

---
+++ frontend/src/main.ts
@@ -0,0 +1,19 @@
+import { createApp } from "vue";
+import "./style.css";
+import App from "./App.vue";
+
+import PrimeVue from "primevue/config";
+import Aura from "@primevue/themes/aura";
+import { createPinia } from "pinia";
+
+const pinia = createPinia();
+const app = createApp(App);
+
+app.use(PrimeVue, {
+  theme: {
+    preset: Aura,
+  },
+});
+
+app.use(pinia);
+app.mount("#app");
